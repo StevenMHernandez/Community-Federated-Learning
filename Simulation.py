3ea18a055bda748dbb48f48a8e49037b808df15f
StevenMHernandez <contact@stevenhdesigns.com>
2019-04-12 09:15:11 -0400

    create nodes based on federated dataset provided by tff

--- Simulation.py
+++ Simulation.py
@@ -1,16 +1,42 @@
+from __future__ import absolute_import, division, print_function
+
+import collections
+from six.moves import range
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras.optimizer_v2 import gradient_descent
+from tensorflow_federated import python as tff
+import random
+
+nest = tf.contrib.framework.nest
+
+np.random.seed(0)
+
+tf.compat.v1.enable_v2_behavior()
+
 from Node import Node
+
+# Maximum number of nodes (datasets can have upwards of 1000s of clients, and thus 1000s of nodes would be created)
+NODE_LIMIT = 25
+# Number of time instances to run the simulation for before evaluation
+SIMULATION_TIME = 10
 
 
 class Simulation:
     @staticmethod
     def main():
-        # Initialize nodes
+        # Initialize nodes based on the given dataset
         nodes = []
-        for n_i in range(1, 10):
-            nodes.append(Node(len(nodes)))
+        train, test = tff.simulation.datasets.emnist.load_data(True, 'storage')
+        node_ids = train.client_ids
+        if len(node_ids) > NODE_LIMIT:
+            random.shuffle(node_ids)
+            node_ids = node_ids[:NODE_LIMIT]
+        for n_i in node_ids:
+            nodes.append(Node(n_i))
 
         # Run simulation
-        for t in range(1, 100):
+        for t in range(1, SIMULATION_TIME + 1):
             print("t: " + str(t))
             for n in nodes:
                 n.move()
@@ -24,6 +50,25 @@ class Simulation:
 
         for n in nodes:
             n.evaluate()
+
+    @staticmethod
+    def preprocess(dataset):
+        NUM_EPOCHS = 5
+        BATCH_SIZE = 10
+        SHUFFLE_BUFFER = 500
+
+        def element_fn(element):
+            return collections.OrderedDict([
+                ('x', tf.reshape(element['pixels'], [-1])),
+                ('y', tf.reshape(element['label'], [1])),
+            ])
+
+        return dataset.repeat(NUM_EPOCHS).map(element_fn) \
+            .shuffle(SHUFFLE_BUFFER).batch(BATCH_SIZE)
+
+    @staticmethod
+    def make_federated_data(client_data, client_ids):
+        return [Simulation.preprocess(client_data.create_tf_dataset_for_client(x)) for x in client_ids]
 
 
 if __name__ == '__main__':
