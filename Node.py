6addb1b596fc8d32a7291928475af2080783d03b
StevenMHernandez <contact@stevenhdesigns.com>
2019-04-12 09:14:28 -0400

    add stub to select random location for nodes on initialization

--- Node.py
+++ Node.py
@@ -9,11 +9,16 @@ class Node:
 
     def __init__(self, identifier):
         self.identifier = identifier
+        self.x, self.y = self.random_initial_location()
         print(str(self.x) + "," + str(self.y))
 
     def sees(self, neighbor):
         return math.sqrt(pow(self.x - neighbor.x, 2) + pow(self.y - neighbor.y, 2)) < self.r
 
+
+    def random_initial_location(self):
+        # TODO
+        return (0,1)
     def move(self):
         # TODO
         pass
